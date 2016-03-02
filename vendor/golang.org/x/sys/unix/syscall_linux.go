0ce455568b0461ba61c218d353d8a0bedc6ba9a1
Joseph Schorr <josephschorr@users.noreply.github.com>
2016-03-02 18:12:33 -0500

    Regenerate godeps

--- vendor/golang.org/x/sys/unix/syscall_linux.go
+++ vendor/golang.org/x/sys/unix/syscall_linux.go
@@ -86,8 +86,8 @@ func Unlink(path string) error {
 
 //sys	unlinkat(dirfd int, path string, flags int) (err error)
 
-func Unlinkat(dirfd int, path string) error {
-	return unlinkat(dirfd, path, 0)
+func Unlinkat(dirfd int, path string, flags int) error {
+	return unlinkat(dirfd, path, flags)
 }
 
 //sys	utimes(path string, times *[2]Timeval) (err error)
@@ -895,6 +895,7 @@ func Getpgrp() (pid int) {
 //sysnb	Setpgid(pid int, pgid int) (err error)
 //sysnb	Setsid() (pid int, err error)
 //sysnb	Settimeofday(tv *Timeval) (err error)
+//sys	Setns(fd int, nstype int) (err error)
 
 // issue 1435.
 // On linux Setuid and Setgid only affects the current thread, not the process.
@@ -921,7 +922,6 @@ func Setgid(uid int) (err error) {
 //sys	Unmount(target string, flags int) (err error) = SYS_UMOUNT2
 //sys	Unshare(flags int) (err error)
 //sys	Ustat(dev int, ubuf *Ustat_t) (err error)
-//sys	Utime(path string, buf *Utimbuf) (err error)
 //sys	write(fd int, p []byte) (n int, err error)
 //sys	exitThread(code int) (err error) = SYS_EXIT
 //sys	readlen(fd int, p *byte, np int) (n int, err error) = SYS_READ
