821e9a83eacde12bba8fed3e62b4a5ffca62b3d2
Joseph Schorr <josephschorr@users.noreply.github.com>
2016-04-11 21:00:23 -0400

    Add support for rkt pull of torrented images
    Fixes #41

---
+++ engine/rkt.go
@@ -0,0 +1,179 @@
+// Copyright 2016 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package engine
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"github.com/appc/spec/discovery"
+	"github.com/spf13/cobra"
+)
+
+// RktEngine defines an engine interface for interacting with rkt.
+type RktEngine struct{}
+
+func (re RktEngine) Name() string {
+	return "rkt"
+}
+
+func (re RktEngine) Title() string {
+	return "rkt"
+}
+
+func (re RktEngine) TorrentHandler() engineTorrentHandler {
+	return &rktTorrentHandler{}
+}
+
+type rktContext struct {
+	signatureUrl *url.URL
+}
+
+// rktTorrentHandler defines an interface for pulling a rkt image via torrent.
+type rktTorrentHandler struct{}
+
+func (rth rktTorrentHandler) DecorateCommand(command *cobra.Command) {}
+
+func (rth rktTorrentHandler) RetrieveTorrents(image string, insecureFlag bool, option layersOption) ([]torrentInfo, interface{}, error) {
+	// Parse the image string.
+	app, err := discovery.NewAppFromString(image)
+	if err != nil {
+		return []torrentInfo{}, nil, err
+	}
+
+	if _, ok := app.Labels["arch"]; !ok {
+		app.Labels["arch"] = runtime.GOARCH
+	}
+	if _, ok := app.Labels["os"]; !ok {
+		app.Labels["os"] = runtime.GOOS
+	}
+
+	// Perform discovery for the image.
+	var insecureOption = discovery.InsecureNone
+	if insecureFlag {
+		insecureOption = discovery.InsecureHTTP
+	}
+
+	log.Printf("Discovering image %v", image)
+	endpoints, _, err := discovery.DiscoverACIEndpoints(*app, nil, insecureOption)
+	if err != nil {
+		return []torrentInfo{}, nil, fmt.Errorf("Could not discover %v: %v", app, err)
+	}
+
+	// Build the URL for the ACI image.
+	aciUrl, err := url.Parse(endpoints[0].ACI)
+	if err != nil {
+		return []torrentInfo{}, nil, fmt.Errorf("Could not download %v: %v", app, err)
+	}
+
+	signatureUrl, err := url.Parse(endpoints[0].ASC)
+	if err != nil {
+		return []torrentInfo{}, nil, fmt.Errorf("Could not download %v: %v", app, err)
+	}
+
+	if insecureFlag {
+		aciUrl.Scheme = "http"
+		signatureUrl.Scheme = "http"
+	}
+
+	// TODO: uncomment and expand properly once `rkt config` has been released.
+	//if credentials.Username != "" {
+	//		aciUrl.User = url.UserPassword(credentials.Username, credentials.Password)
+	//      signatureUrl.User = url.UserPassword(credentials.Username, credentials.Password)
+	//	}
+
+	log.Printf("Downloading torrent for image %v", image)
+	torrent := torrentInfo{
+		id:          "aci",
+		torrentPath: aciUrl.String(),
+		title:       image,
+	}
+
+	return []torrentInfo{torrent}, rktContext{signatureUrl}, nil
+}
+
+func (rth rktTorrentHandler) LoadImage(image string, downloadInfo downloadTorrentInfo, ctx interface{}) error {
+	// Wait for the torrent to complete.
+	<-downloadInfo.CompleteChannel
+
+	// Download the signature.
+	log.Printf("Downloading signature for image %v", image)
+	aciPath, _ := downloadInfo.TorrentPaths.Get("aci")
+	signaturePath := fmt.Sprintf("%s.aci.asc", aciPath)
+	err := downloadFile(ctx.(rktContext).signatureUrl, signaturePath)
+	if err != nil {
+		return fmt.Errorf("Could not download signature for image %v: %v", image, err)
+	}
+
+	// Load the image into rkt via a fetch of the local file.
+	log.Printf("Loading image %v", image)
+	aciLocalPath := url.URL{
+		Scheme: "file",
+		Path:   aciPath.(string),
+	}
+
+	cmd := exec.Command("rkt", "fetch", aciLocalPath.String(), "--trust-keys-from-https=true")
+	cmdReader, err := cmd.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("Could not load image %v into rkt: %v", image, err)
+	}
+
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	err = cmd.Start()
+	if err != nil {
+		return fmt.Errorf("Could not load image %v into rkt: %v", image, err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return fmt.Errorf("Could not load image %v into rkt: %v", image, err)
+	}
+
+	return nil
+}
+
+func downloadFile(url *url.URL, filePath string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	resp, err := http.Get(url.String())
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
